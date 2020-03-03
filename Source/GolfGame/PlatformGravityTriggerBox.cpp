df46ac977df027d5d09220a4296eb5e2cc9d4ffe
dnisyndrum <dnisyndrum@yahoo.com>
2020-03-03 10:36:16 -0800

    Added a customer trigger box actor
    Customer trigger box currently only prints debug test to the screen for the actor that enters and leaves the trigger box. I can select which specific actor I want to be effected.

---
+++ Source/GolfGame/PlatformGravityTriggerBox.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlatformGravityTriggerBox.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/Engine.h"
+
+APlatformGravityTriggerBox::APlatformGravityTriggerBox()
+{
+    //Register Events
+    OnActorBeginOverlap.AddDynamic(this, &APlatformGravityTriggerBox::OnOverlapBegin);
+    OnActorEndOverlap.AddDynamic(this, &APlatformGravityTriggerBox::OnOverlapEnd);
+}
+
+void APlatformGravityTriggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+	DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Green, true, -1, 0, 5);
+}
+
+void APlatformGravityTriggerBox::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+    if (OtherActor && OtherActor != this && OtherActor)
+    {
+        if (GEngine)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, TEXT("Overlap Begin"));
+            GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, FString::Printf(TEXT("Overlapping Actor = %s"), *OverlappedActor->GetName()));
+        }
+    }
+}
+
+void APlatformGravityTriggerBox::OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+    if (OtherActor && OtherActor != this && OtherActor == SpecificActor)
+    {
+        if (GEngine)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, TEXT("Overlap Ended"));
+            GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, FString::Printf(TEXT("%s has left the Trigger Box"), *OtherActor->GetName()));
+        }
+    }
+}
