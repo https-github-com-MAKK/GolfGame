66b81504162134d3d285aba3ff1d993afc4b3fb7
dnisyndrum <dnisyndrum@yahoo.com>
2020-03-24 10:39:48 -0700

    Created platform static mesh
    Created a static mesh for skinny and wide platforms. This will allow a .cpp class to back each of the platforms allowing for runtime alteration of their relative locations.

---
+++ Source/GolfGame/Platform_Moving.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Engine/Engine.h"
+#include "Platform_Moving.h"
+
+// Sets default values
+APlatform_Moving::APlatform_Moving()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	VisualMesh->SetupAttachment(RootComponent);
+	VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	
+}
+
+// Called when the game starts or when spawned
+void APlatform_Moving::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(!ControllerSwitch)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Red, TEXT("Controller not assigned"));
+	}
+	
+}
+
+// Called every frame
+void APlatform_Moving::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(testAction)
+	{
+		FVector NewLocation = GetActorLocation();
+		float RunningTime = GetGameTimeSinceCreation();
+		float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+		NewLocation.Z += DeltaHeight * ScaleFactor;       
+		SetActorLocation(NewLocation);
+	}
+	
+}
+
---
+++ Source/GolfGame/Platform_Moving.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Platform_Moving.generated.h"
+
+UCLASS()
+class GOLFGAME_API APlatform_Moving : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatform_Moving();
+
+	//must specify controller for platform to decide whether it in motion
+	UPROPERTY(EditAnywhere)
+		class AActor* ControllerSwitch;
+
+	//skinny or wide platform mesh
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* VisualMesh;
+
+	//factor for which the platform will move up and down
+	UPROPERTY(EditAnywhere)
+		float ScaleFactor = 50.0;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	//to test platform movement without connecting a controller
+	const bool testAction = true;
+	
+};
