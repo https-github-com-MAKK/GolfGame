e5711a0458c2cce0000bf048c331fc56bb7b8711
dnisyndrum <dnisyndrum@yahoo.com>
2020-04-23 12:32:27 -0700

    Added hall, ball chute, and easter eggs
    Created entrance hall way at start of level one and created a static mesh from it. Others must still add moving door. Added chute in first landing for ball to roll out of when play begins. Added two easter three easter eggs.

---
+++ Source/GolfGame/MovementType.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "MovementType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLFGAME_API UMovementType : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+};
