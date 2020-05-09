50bd4b75b11995cf841fc661fd147b4843e59e7a
dnisyndrum <dnisyndrum@yahoo.com>
2020-05-08 18:35:52 -0700

    Added particle effect to ball, WIP
    Added plue spark partible effect to ball. Needs refinement, currently always on, but going to alter it so that is only activates when impacting a surface.

--- Source/GolfGame/Ball.cpp
+++ Source/GolfGame/Ball.cpp
@@ -10,23 +10,34 @@ ABall::ABall()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	/*VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ball Mesh"));
-	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
-	VisualMesh->SetupAttachment(SphereVisual);*/
 
-	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Visual Mesh"));
-	RootComponent = VisualMesh;
+	SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Visual Mesh"));
+	RootComponent = SphereVisual;
 
-	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
-	SphereComponent->InitSphereRadius(40.0f);
-	SphereComponent->SetupAttachment(RootComponent);
-
-	BallMaterial = CreateDefaultSubobject<UMaterial>(TEXT("Ball Material"));
-}
-
-void ABall::AddForce()
-{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Visual"));
 	
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABall::OnOverlapBegin);
+	//SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ABall::OnOverlapEnd);
+	
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+    if (SphereVisualAsset.Succeeded())
+    {
+        SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+        SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+        SphereVisual->SetWorldScale3D(FVector(0.8f));
+    }
+
+    // Create a particle system that we can activate or deactivate
+    SparksParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+    SparksParticleSystem->SetupAttachment(RootComponent);
+    SparksParticleSystem->bAutoActivate = true;
+    SparksParticleSystem->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+    static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Sparks.P_Sparks"));
+    if (ParticleAsset.Succeeded())
+    {
+        SparksParticleSystem->SetTemplate(ParticleAsset.Object);
+    }
+
 }
 
 // Called when the game starts or when spawned
@@ -41,5 +52,15 @@ void ABall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ABall::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//SparksParticleSystem->ToggleActive();
+}
+
+void ABall::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//SparksParticleSystem->ToggleActive();
 }
 
--- Source/GolfGame/Ball.h
+++ Source/GolfGame/Ball.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/SphereComponent.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "Ball.generated.h"
 
 UCLASS()
@@ -16,7 +17,6 @@ public:
 	// Sets default values for this actor's properties
 	ABall();
 
-	void AddForce();
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,15 +25,20 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
 	UPROPERTY()
 		USphereComponent* SphereComponent;
 	
 	UPROPERTY(VisibleAnywhere)
-		UStaticMeshComponent* VisualMesh;
+		UStaticMeshComponent* SphereVisual;
 
-	UPROPERTY(EditAnywhere)
-		class UMaterial* BallMaterial;
-	
+	UPROPERTY()
+		UParticleSystemComponent* SparksParticleSystem;
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 };
