dcc315f81eb6cb37b877b9525f32100920efe9ce
MaxIsBack <57336544+MaxIsBack@users.noreply.github.com>
2020-05-15 14:14:25 -0700

    Update lighting and triggers

--- Source/GolfGame/WindTriggerBox.cpp
+++ Source/GolfGame/WindTriggerBox.cpp
@@ -20,22 +20,37 @@ AWindTriggerBox::AWindTriggerBox()
 
 void AWindTriggerBox::BeginPlay() 
 {
+	IsUsable = false;
 	Super::BeginPlay();
 
 }
 
 void AWindTriggerBox::OnOverlapBegin(class AActor* OverlappedActor,  class AActor* OtherActor) 
 {
-		
+	if(IsUsable)
+		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White, TEXT("ISUSable true"));
+
+	if (!IsUsable)
+		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White, TEXT("ISUSable false"));
+
+	if (IsTriggered)
+		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White, TEXT("ISTriggered true"));
+
+	if (!IsTriggered)
+		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White, TEXT("ISTriggered true"));
 
 	 cameraForward = GetActorForwardVector();
-	 if (OtherActor && OtherActor != this && OtherActor == Ball && WindOn == false)
+	 if (OtherActor && OtherActor != this && OtherActor == Ball && !WindOn && ((IsTriggered && IsUsable) || (!IsTriggered && !IsUsable)))
 	 {
-	
 		 AddForce(OverlappedActor, OtherActor);
-
 		 
 	 }
+	/* cameraForward = GetActorForwardVector();
+	 if (OtherActor && OtherActor != this && OtherActor == Ball && !WindOn)
+	 {
+		 AddForce(OverlappedActor, OtherActor);
+
+	 }*/
 }
 
 void AWindTriggerBox::AddForce( class AActor* OverlappedActor, class AActor* OtherActor)
@@ -62,6 +77,7 @@ void AWindTriggerBox::OnOverlapEnd(AActor* OverlappedActor, AActor* OtherActor)
 	if (GetWorld()->GetTimerManager().IsTimerActive(InputADelayManager)) 
 	{
 		GetWorld()->GetTimerManager().ClearTimer(InputADelayManager);
+		WindOn = false;
 		GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White, TEXT("ForceStopped"));
 
 	}
