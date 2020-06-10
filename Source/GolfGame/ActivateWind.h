744f10065e72f1ab043023dbd4b3afea0b8d280e
MaxIsBack <57336544+MaxIsBack@users.noreply.github.com>
2020-06-09 22:31:41 -0700

    documentation

--- Source/GolfGame/ActivateWind.h
+++ Source/GolfGame/ActivateWind.h
@@ -32,66 +32,104 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	/**\brief boolean to specify how many trigger boxes.*/
+
 	UPROPERTY(EditAnywhere)
 		bool ToggleTwoWindTriggerBoxes;
 	
+	/**\brief Visible mesh representing the actor. Set in UE4 editor.*/
+
 	UPROPERTY(VisibleAnywhere)
 		class UStaticMeshComponent* MyMesh;
 
+	/**\brief Green material should be selected, represents the switch in the on state. Set in
+	 * UE4 editor.
+	 */
 	UPROPERTY(EditAnywhere)
 		class UMaterial* OnMaterial;
 
+	/**\brief Red material should be selected, represents the switch in the off state. Set in
+	 * UE4 editor.
+	 */
 	UPROPERTY(EditAnywhere)
 		class UMaterial* OffMaterial;
 
+	/**\brief Box component for the overlapping section of the switch, where the ball will make
+	 *contact and toggle switch properties.*/
 	UPROPERTY()
 		class UBoxComponent* MyBoxComponent;
 
+	/**\brief .*/
 	UPROPERTY(EditAnywhere)
 		bool OnAtStart = false;
 
+	/**\brief Toggles properties when ball overlaps the actor. Sets CanBeHit to false. Toggles associated
+	 * light's visibility, whether an associated platform is moving and whether an associated elevator can
+	 * move.
+	 */
 	UFUNCTION()
 		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	/**\brief The WindTriggerBox that needs to be turned on or off.*/
 	UPROPERTY(EditAnywhere)
 		class AWindTriggerBox* AssociatedWindBox;
 
+	//
+	/**\brief The ball that impact the switch to turn it on or off. Specific ball set in UE4 editor.*/
 	UPROPERTY(EditAnywhere)
 		class ABall* Ball;
 
+	/**\brief Selected light source to toggle visibility. Set in UE4 editor.*/
 	UPROPERTY(EditAnywhere)
 		class ARectLight* AssociatedRectlight;
 
+	/**\brief left side wind sound*/
 	UPROPERTY(EditAnywhere)
 		class AAmbientSound* WindSoundLeft;
 
+	/**\brief Right side wind sound.*/
 	UPROPERTY(EditAnywhere)
 		class AAmbientSound* WindSoundRight;
 
+	/**\brief timer for the wind force.*/
 	FTimerHandle MemberTimerHandle;
 
 
 	
 private:
 
+	/**\brief Called to turn the wind off if it is on
+		 */
 	void ToggleWind();
 
+	/**\brief Called to turn the wind on if it is off
+		 */
 	void ToggleWindOff();
 
+	/**\brief Called to turn the the spotlight on or off
+		 */
 	void ToggleSpotlight();
+
+	/**\brief sets CanBeHit variable to true.*/
 
 	void SetCanBeHit();
 
+	/**\brief Called to turn the wind sound on or off
+		 */
 	void ToggleWindSound();
 
+	/**\brief enum for switches to internally represent whether they are in an on or off state.*/
+
 	enum SwitchState { On, Off };
-	SwitchState CurrentSwitchState = Off;
-	SwitchState CurrentSpotlightState = Off;
+
+	/**\brief Variable to decide whether or not this actor can be hit.*/
 
 	bool CanBeHit;
 
-	SwitchState CurrentWindSoundLeft = On;
+	/**\brief Current state of the the switch, on of off.*/
+	SwitchState CurrentSwitchState = Off;
 
-	SwitchState CurrentWindSoundRight = Off;
+	/**\brief Current state of the light, on or off*/
+	SwitchState CurrentSpotlightState = Off;
 
 };
--- Source/GolfGame/WindTriggerBox.h
+++ Source/GolfGame/WindTriggerBox.h
@@ -22,55 +22,56 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	void AddForce(class AActor* OverlappedActor, class AActor* OtherActor);
-
+	/**\brief Handles the delay for the tick*/
 	FTimerHandle InputADelayManager;
 
 public:
-
+	/**\brief Function for the timer tick and what happens in it.*/
 	virtual void Tick();
 
 	// constructor sets default values for this actor's properties
 	AWindTriggerBox();
 
-	// overlap begin function
+	/**\brief Function for adding the force the the ball during the tick.*/
+	void AddForce(class AActor* OverlappedActor, class AActor* OtherActor);
+
+	/**\brief Function of when the ball enters the box.*/
 	UFUNCTION()
 		void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
 
-	// overlap end function
+	/**\brief Function of when the ball exits the box.*/
 	UFUNCTION()
 		void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
 
+	/**\brief The ball that impact the switch to turn it on or off. Specific ball set in UE4 editor.*/
 	UPROPERTY(EditAnywhere)
 		class ABall* Ball;
-
+	
+	/**\brief Integer force to be added to the ball when add force is applied.*/
 	UPROPERTY(EditAnywhere)
 		int force = 1000;
 
+	/**\brief Boolean to tell the trigger box whether or not wind has to be activated or not*/
 	UPROPERTY(EditAnywhere)
 		bool IsTriggered = false;
 
+	/**\brief Boolean to tell the wind trigger box to turn on or not (can be accessed by a blueprint).*/
 	UPROPERTY(BlueprintReadWrite)
 		bool IsUsable;
 
 
-	void SetIsUsable(bool choice)
-	{
-		IsUsable = choice;
-	}
-
-	bool GetIsUsable() 
-	{
-		return IsUsable;
-	}
 
 private:
+
+	/**\brief Boolean to tell the wind trigger box on whether or not the wind is currently on.*/
 		bool WindOn;
 
-
+		/**\brief Vector of where the wind should point.*/
 		FVector cameraForward;
 
+		/**\brief Handler for the wind tick.*/
 		FTimerHandle loopHandle;
 
+		/**\brief Mesh for inherited ball object.*/
 		UStaticMeshComponent* meshRootComp;
 };
